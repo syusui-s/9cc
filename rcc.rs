1d513edd48a8cb98f58bc9c0b2e44fe4630d726e
Shusui MOYATANI <syusui.s@gmail.com>
2018-12-06 23:25:31 +0900

    パーサが完成

--- rcc.rs
+++ rcc.rs
@@ -1,5 +1,7 @@
 use std::iter::Peekable;
+use std::slice::Iter;
 use std::str::Chars;
+use std::boxed::Box;
 
 #[derive(Debug, Clone, PartialEq)]
 enum Token {
@@ -64,13 +66,118 @@ fn tokenize(input: &str) -> Result<Vec<Token>, TokenizeError> {
     Ok(result)
 }
 
+#[derive(Debug, Clone, PartialEq)]
+enum Program {
+    Expr(::Expr),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+enum Expr {
+    Add(Term, Box<Expr>),
+    Sub(Term, Box<Expr>),
+    Term(::Term),
+}
+
+/*
+enum Multiply {
+    Mul(Term, Box<Multiply>),
+    Div(Term, Box<Multiply>),
+    Term(Term),
+}
+*/
+
+#[derive(Debug, Clone, PartialEq)]
+enum Term {
+    Int64(i64),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+enum ParseError {
+    UnexpectedToken(Token),
+    UnexpectedEOF,
+}
+
+fn parse(tokens: &[Token]) -> Result<Program, ParseError> {
+    let mut peekable = tokens.iter().peekable();
+
+    parse_program(&mut peekable)
+}
+
+fn parse_program(tokens: &mut Peekable<Iter<Token>>) -> Result<Program, ParseError> {
+    let result = Program::Expr(parse_expr(tokens)?);
+
+    Ok(result)
+}
+
+fn parse_expr(tokens: &mut Peekable<Iter<Token>>) -> Result<Expr, ParseError> {
+    use Token::*;
+    use Expr::*;
+
+    let lhs = parse_term(tokens)?;
+
+    if let Some(&tok) = tokens.peek() {
+        let expr = match tok {
+            Plus  => {
+                tokens.next();
+                Add(lhs, Box::new(parse_expr(tokens)?))
+            },
+            Minus => {
+                tokens.next();
+                Sub(lhs, Box::new(parse_expr(tokens)?))
+            },
+            tok => return Err(ParseError::UnexpectedToken(tok.clone())),
+        };
+
+        Ok(expr)
+    } else {
+        Ok(Expr::Term(lhs))
+    }
+}
+
+fn parse_term(tokens: &mut Peekable<Iter<Token>>) -> Result<Term, ParseError> {
+    use Token::*;
+
+    if let Some(&tok) = tokens.peek() {
+        let term = match tok {
+            Int64(v) => {
+                tokens.next();
+                Term::Int64(*v)
+            },
+            tok => return Err(ParseError::UnexpectedToken(tok.clone())),
+        };
+
+        Ok(term)
+    } else {
+        Err(ParseError::UnexpectedEOF)
+    }
+}
+
 fn main() {
-    match tokenize("1+2") {
+    match tokenize("221+ 212") {
         Ok(ok) =>
             println!("OK: {:?}", ok),
         Err(err) =>
             println!("ERROR: {:?}", err),
     }
+}
+
+#[test]
+fn test_parse() {
+    use Token::*;
+
+    let input = vec![Int64(221), Plus, Int64(212)];
+
+    let result = parse(&input);
+    let expected = Program::Expr(
+        Expr::Add(
+            Term::Int64(221),
+            Box::new(Expr::Term(Term::Int64(212)))
+        )
+    );
+
+    println!("{:?}", result);
+
+    assert!(result == Ok(expected));
 }
 
 #[test]
